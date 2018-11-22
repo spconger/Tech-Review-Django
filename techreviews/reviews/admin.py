ab5b1d7357dda8fb386b07d60a9ce110435730a0
Steve Conger <steve@spconger.com>
2018-11-22 14:09:52 -0800

    Second Commit
    Added and migrated model. added model reference in admin.py created superuser

--- techreviews/reviews/admin.py
+++ techreviews/reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TechType, TechProduct, TechReview
 
 # Register your models here.
+admin.site.register(TechType)
+admin.site.register(TechProduct)
+admin.site.register(TechReview)
--- techreviews/reviews/models.py
+++ techreviews/reviews/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
 # Create your models here.
+class TechType(models.Model):
+    typename = models.CharField(max_length=255)
+    typedescription=models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.typename
+    
+    class Meta:
+        db_table='techtype'
+    
+class TechProduct(models.Model):
+    productname=models.CharField(max_length=255)
+    techtype=models.ForeignKey(TechType, on_delete=models.CASCADE)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    entryDate=models.DateField()
+    productURL=models.URLField()
+    productdescription=models.TextField()
+
+    def __str__(self):
+        return self.productname
+    
+    class Meta:
+        db_table='techproduct'
+
+class TechReview(models.Model):
+    reviewtitle=models.CharField(max_length=255)
+    product=models.ForeignKey(TechProduct, on_delete=models.CASCADE)
+    reviewdate=models.DateTimeField()
+    reviewers=models.ManyToManyField(User)
+    reviewrating = models.SmallIntegerField()
+    reviewText = models.TextField()
+
+    def __str__(self):
+        return self.reviewtitle
+
+    class Meta:
+        db_table='techreview'
